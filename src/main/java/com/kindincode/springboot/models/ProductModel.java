d396f1de56f82fb349f8f0ef358de6a16ebd1943
denisiorodrigues <denisio@ymail.com>
2024-04-11 19:59:01 -0300

    feat: producct entity + postgresql database cinfiguration

--- src/main/java/com/kindincode/springboot/models/ProductModel.java
+++ src/main/java/com/kindincode/springboot/models/ProductModel.java
@@ -1,4 +1,19 @@
 package com.kindincode.springboot.models;
 
-public class ProductModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class ProductModel implements Serializable {
+    private static final long serialVersionID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
 }
